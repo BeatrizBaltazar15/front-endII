7687e795e12ca1284ea0986a7aba07d244a38967
BeatrizBaltazar15 <bebaltazar891@gmail.com>
2025-02-05 13:26:23 -0400

    teste

--- front-endii/src/app/page.js
+++ front-endii/src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     
 
       <div>
-        <h1 className={styles.h1}>Home</h1>
+        <h1 className={styles.h1}>Homee</h1>
         <p></p>
         <Image className={styles.img} src='/images/' alt=" " width={625} height={500} />
       
